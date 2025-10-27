a8da2510abab7edbf820a9f96fe8e04003f03721
Rishit Garg <rishitalig@gmail.com>
2025-10-27 22:24:04 +0530

    Python Day1
    learning syntax

---
+++ 01_Syntax/2_Variables.py
@@ -0,0 +1,11 @@
+name = ("Rishit Garg")
+print(name)
+print(type(name))
+
+marks = 130
+marks2 = ("345")
+
+num_marks2 = int(marks2) # Convert String to int
+
+print(num_marks2)
+print(type(num_marks2)) #
---
+++ 01_Syntax/4_escape_seq.py
@@ -0,0 +1,1 @@
+print("Hello world\ni love u world\\")
