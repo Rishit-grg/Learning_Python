cfff2f3b1c578f338eddc689b36b8807c189f557
Rishit Garg <rishitalig@gmail.com>
2025-11-06 23:13:00 +0530

    Folder Organisation

--- 02_Control Flow and Loops/P4.1.py
+++
@@ -1,6 +0,0 @@
-a = input("Enter Password: ")
-
-while a!=("CorrectPass123"):
-    a = input("Enter Password: ")
-    print("Wrong Password")
-    
---
+++ CWH ex/02_Control Flow and Loops/P4.1.py
@@ -0,0 +1,8 @@
+b = "RGRG"
+while True:
+    a = input("Enter Password: ")
+    if a != b:
+        print("Wrong Password")
+    else:
+        print("Welcome")
+        break
